c30a9a329b9b471f2e6ce744577ae029432b9374
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-03-25 13:18:23 +0000

    Trying to clean up code and split into array.

--- src/CS1003P4.java
+++ src/CS1003P4.java
@@ -1,10 +1,11 @@
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class CS1003P4 {
     public static void main(String[] args) {
@@ -24,13 +25,39 @@ public class CS1003P4 {
     }
 
     public void search(String directory, String searchTerm, double similarity) {
-        //System.out.println(directory + "\n" + searchTerm + "\n" + similarity);
         String[] files = new File(directory).list();
-        System.out.println(Arrays.toString(files));
-
+        try {
+            if (files == null || files.length == 0) {
+                throw new FileNotFoundException("\"" + directory + "\" does not contain any files!");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         SparkConf conf = new SparkConf().setAppName("SparkPractical").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        //JavaRDD<String> line = sc.textFile(files[0]);
-        //System.out.println(line.take(1));
+        sc.setLogLevel("ERROR");
+        for (String file : files) {
+            JavaPairRDD<String, String> wholeText = sc.wholeTextFiles(directory + "/" + file);
+            String line = wholeText.take(1).toString();
+            String[] words = splitWords(line);
+        }
+    }
+
+    public String[] splitWords(String line) {
+        line = line.substring(line.indexOf(".txt") + 6);
+        line = line.replaceAll("[^a-zA-Z0-9]", " ");
+        line = line.toLowerCase();
+        return line.split("[ \t\n\r]");
+    }
+
+    public HashSet<String> createBigram(String word) {
+        HashSet<String> bigram = new HashSet<String>();
+        for (int i = 0; i < word.length() - 1; i ++) {
+            String letter1 = "" + word.charAt(i);
+            String letter2 = "" + word.charAt(i + 1);
+            String bothLetters = letter1 + letter2;
+            bigram.add(bothLetters);
+        }
+        return bigram;
     }
 }
